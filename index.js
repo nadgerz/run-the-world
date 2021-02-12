8f6e43a2fdd7dcd9a42e7a2c8b925a35074961e5
neslihankara <neslihankara40@gmail.com>
2021-02-12 11:28:22 +0300

    Fix classes and add admin

--- index.js
+++ index.js
@@ -2,88 +2,72 @@ class Race {
     runners = []
     
     constructor(name, kilometers = 42, terrain = "asphalt", requiredRunnerAge, requiredRunnerGender, startTime, duration){
-        this.name = name;
-        this.kilometers = kilometers;
-        this.terrain = terrain;
-        this.requiredRunnerAge = requiredRunnerAge;
-        this.requiredRunnerGender = requiredRunnerGender;
-        this.startTime = startTime;
-        this.duration = duration;
+        this.name = name
+        this.kilometers = kilometers
+        this.terrain = terrain
+        this.requiredRunnerAge = requiredRunnerAge
+        this.requiredRunnerGender = requiredRunnerGender
+        this.startTime = startTime
+        this.duration = duration
     }
 
-    start(){
-        //start the time. id setter to runner
-        //marathon doesnt care about runner
+    start(timer){
+        //start the timer or km
+        
         console.log("get ready!")
     }
 
-    end(endingTime) {
+    end(timer) {
+        //end the timer or km 
+
         console.log("it is done!")
     }
 }
 
 class Runner {
-    //constructor ekle
-
     id = Math.round(Math.random() * 100000000)
     
-    reqRunner = {
-        age: 0,
-        gender: ""
+    constructor(name, age, gender) {
+        this.name = name
+        this.age = age
+        this.gender = gender
     }
 
-    name = ""
-    attemptedraces = []
-    finishedraces = []
-    
-    //apply(race) method, call the admit method of the race (create it first)
-    //inside apply's body, we can have race.admit(this)
+    applyRace(race) {
+        //fix this bug
+        if Admin.admit(this, race) console.log(`you are admitted to ${race.name}`)
+    }
 
     joinRace(race) {
-        //check it on admit
-        if (race.reqRunner !== this.reqRunner) throw new Error('this race is not right for you.')
-        
-        this.attemptedraces.push(race)
         console.log("you got this!")
-
     }
 
     quitrace() {
-    
         console.log("Hey don't be sad <3")
-
-        //NOremove the marathon from the array
     }
 
     endrace(race) {
-        //not pop find another way
-        this.attemptedraces.pop(race)
-        this.finishedraces.push(race)
         console.log("congrats!!")
     }
 }
 
 //status fixed string 
 
-const race = new Race()
-race.kilometers = 26
-race.gender = "female"
-race.age = 45
+class Admin {
+    /***
+     * Applies the required controls to admit a user to a race
+     * @constructor
+     */
+    constructor(user, race) {
+        this.user = user
+        this.race = race
+    }
 
-const nes = new Runner()
-nes.age = 45
-nes.gender = "female"
-nes.name = "nes"
+     //apply(race) method, call the admit method of the race (create it first)
+    //inside apply's body, we can have race.admit(this)
 
-nes.joinRace(race)
-race.start()
-console.log(race.runners)
-
-
-//console.log(nes.joinrace(a))
-/*
-TypeError: Cannot read private member #kilometers 
-from an object whose class did not declare it
-console.log(Race.prototype.kilometers)
-*/
+    static admit(user, race) {
+        return user.age === race.age && user.gender === race.gender
+    }
+}
 
